3a458afd54ea2277f6adaded3d09fabdca841f46
PhongLe7de <phongle260260@gmail.com>
2024-04-25 10:01:14 +0300

    util function

--- FrontEnd/src/diceRolling.js
+++ FrontEnd/src/diceRolling.js
@@ -1,10 +1,18 @@
-const handleDiceRolling = (player) =>{
-    const player = player
-    const randomDiceValue = Math.floor(Math.random()* 6 + 1)
-    const randomDiceImg = player = 1 ? "diceRed"+randomDiceValue+".png" : "diceBlue"+randomNumber+".png" 
-    const getDiceImgElement = document.getElementsByClassName("diceValueImage")
-    getDiceImgElement.setAttribute("scr",`assets/img/dice/${randomDiceImg}`)
-    return randomDiceValue
-} 
+import axios from "axios";
 
-export default handleDiceRolling
+const URL = ''; //waiting for URL from BackEnd
+
+export const handleDiceRolling = (player) => {
+  const player = player;
+  const randomDiceValue = Math.floor(Math.random() * 6 + 1);
+  const randomDiceImg = (player = 1
+    ? "diceRed" + randomDiceValue + ".png"
+    : "diceBlue" + randomNumber + ".png");
+  const getDiceImgElement = document.getElementsByClassName("diceValueImage");
+  getDiceImgElement.setAttribute("scr", `assets/img/dice/${randomDiceImg}`);
+  return randomDiceValue;
+};
+
+
+
+
