415c6a5e7bd551fb33440588d52e30a000b0e1d0
PhongLe7de <phongle260260@gmail.com>
2024-04-28 14:36:11 +0300

    update animation stylecss

--- FrontEnd/src/util/getData.js
+++ FrontEnd/src/util/getData.js
@@ -1,15 +1,20 @@
-const URL = ''; //waiting for URL from BackEnd
+const proxy = 'https://users.metropolia.fi/~ilkkamtk/proxy.php?url='
+const URL_API = proxy + 'http://localhost:3000/gameboard'; //waiting for URL from BackEnd
 
-const fetchAPI = async (URL) =>{
+const fetchAPI = async (URL) => {
     try {
-        const response = await fetch(URL,{
-            method:'GET',
-            headers:{
-                'Content-type':'application/json'
-            }
+        const response = await fetch(URL, {
+            method: 'GET'
         })
-        response ? console.log(response) : console.log('Fetch API FAIL');
+        if (response.ok) {
+            console.log(response);
+            const result = await response.json()
+            console.log(result);
+        } else { 
+            console.log('Fetch API FAIL')
+        }
     } catch (error) {
         console.log(error);
-    }   
+    }
 }
+fetchAPI(URL_API)
