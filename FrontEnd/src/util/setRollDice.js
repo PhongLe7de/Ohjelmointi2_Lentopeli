5a12c57f7edcecf533d103dbc727eb768b4f7ce9
nealukumies <nea.lukumies@metropolia.fi>
2024-05-03 10:33:30 +0300

    korttibisnes

--- FrontEnd/src/util/setRollDice.js
+++ FrontEnd/src/util/setRollDice.js
@@ -27,21 +27,20 @@ const handleDiceRolling = async (player) => {
 
   if (checkClass[0] === 'open') {
 
-    currentPlayer === 0 ? playerSkipTurns = player01Data.effect : player02Data.effect
+    playerSkipTurns = currentPlayer === 0 ? player01Data.effect : player02Data.effect;
     console.log(listPlayer[currentPlayer]);
     console.log('start', playerSkipTurns);
     if (playerSkipTurns > 0) {
 
       currentPlayer === 0
-        ? player01Data.effect = playerSkipTurns
-        : player02Data.effect = playerSkipTurns
-      playerSkipTurns = playerSkipTurns - 1
+        ? player01Data.effect--
+        : player02Data.effect--
     }
     else {
       const randomDiceValue = Math.floor(Math.random() * 6 + 1);
       const randomDiceImg = (player === 0
-        ? "diceRed" + randomDiceValue + ".png"
-        : "diceBlue" + randomDiceValue + ".png");
+          ? "diceRed" + randomDiceValue + ".png"
+          : "diceBlue" + randomDiceValue + ".png");
       const getDiceImgElement = document.querySelector(".dice-img");
       getDiceImgElement.src = `assets/img/dice/${randomDiceImg}`
 
@@ -50,9 +49,13 @@ const handleDiceRolling = async (player) => {
         value: randomDiceValue
       }
       const response = await handlePostData(data)
+      console.log(response)
       const playerData = {
+        icao: response['ident'],
         score: response['update_score']['score'],
-        effect: response['get_effect']['effect_skip_turns'],
+        co_effect: response['co_card_effect'],
+        surprise_effect: response['surprise_effect'],
+        // effect: response['final_effect_value']['effect_skip_turns'],
         mainland: response['space']['continent'],
         co2: response['co_card'],
         surprise: response['surprise_card']
@@ -62,13 +65,140 @@ const handleDiceRolling = async (player) => {
       const playerMainland = playerData.mainland
       if (currentPlayer === 0) {
         player01Data.score = playerData.score
-        player01Data.effect = playerData.effect
+        switch (playerData.surprise_effect) {
+          case 1:
+            player02Data.effect++;
+            break;
+          case 2:
+            player01Data.effect++;
+            break;
+          case 3:
+            data = {
+              currentPlayer: listPlayer[currentPlayer],
+              value: -3
+            }
+            const back = await handlePostData(data)
+            console.log(back)
+            break;
+          case 4:
+            data = {
+              currentPlayer: listPlayer[currentPlayer],
+              value: 3
+            }
+            const forward = await handlePostData(data)
+            console.log(forward)
+            break;
+          default:
+            // Handle other cases if needed
+            break;
+        }
+        switch (playerData.co_effect) {
+          case 1:
+            player02Data.effect++;
+            break;
+          case 2:
+            player01Data.effect++;
+            break;
+          case 3:
+            data = {
+              currentPlayer: listPlayer[currentPlayer],
+              value: -3
+            }
+            const back = await handlePostData(data)
+            console.log(back)
+            break;
+          case 4:
+            data = {
+              currentPlayer: listPlayer[currentPlayer],
+              value: 3
+            }
+            const forward = await handlePostData(data)
+            console.log(forward)
+            break;
+          default:
+            // Handle other cases if needed
+            break;
+        }
         const checkFunction = player01Data.mainland.includes(playerMainland)
-        if (checkFunction === false) { player01Data.mainland.push(playerMainland) }
+        if (checkFunction === false) {
+          player01Data.mainland.push(playerMainland)
+        }
 
       } else {
-        player02Data.score = playerData.score
-        player02Data.effect = playerData.effect
+        switch (playerData.surprise_effect) {
+          case 1:
+            data = {
+              currentPlayer: listPlayer[currentPlayer],
+              value: 1
+            }
+            handleSkipTurns(data)
+            // player01Data.effect++;
+            break;
+          case 2:
+            data = {
+              currentPlayer: listPlayer[currentPlayer],
+              value: 1
+            }
+            handleSkipTurns(data)
+            // player02Data.effect++;
+            break;
+          case 3:
+            data = {
+              currentPlayer: listPlayer[currentPlayer],
+              value: -3
+            }
+            const back = await handlePostData(data)
+            console.log(back)
+            break;
+          case 4:
+            data = {
+              currentPlayer: listPlayer[currentPlayer],
+              value: 3
+            }
+            const forward = await handlePostData(data)
+            console.log(forward)
+            break;
+          default:
+            // Handle other cases if needed
+            break;
+        }
+      switch (playerData.co_effect) {
+        case 1:
+          data = {
+            currentPlayer: listPlayer[currentPlayer],
+            value: 1
+          }
+          handleSkipTurns(data)
+          // player01Data.effect++;
+          break;
+        case 2:
+          data = {
+            currentPlayer: listPlayer[currentPlayer],
+            value: 1
+          }
+          handleSkipTurns(data)
+          // player02Data.effect++;
+          break;
+        case 3:
+          data = {
+            currentPlayer: listPlayer[currentPlayer],
+            value: -3
+          }
+          const back = await handlePostData(data)
+          console.log(back)
+          break;
+        case 4:
+          data = {
+            currentPlayer: listPlayer[currentPlayer],
+            value: 3
+          }
+          const forward = await handlePostData(data)
+          console.log(forward)
+          break;
+        default:
+          // Handle other cases if needed
+          break;
+      }
         const checkFunction = player02Data.mainland.includes(playerMainland)
         if (checkFunction === false) { player02Data.mainland.push(playerMainland) }
       }
@@ -96,7 +226,7 @@ const handleDiceRolling = async (player) => {
       }
     })
     console.log(player01Data.score);
-    //score 
+    //score
     const scoreP1 = document.getElementById('p1-points')
     const scoreP2 = document.getElementById('p2-points')
     scoreP1.innerHTML = `${player01Data.score}`
@@ -110,8 +240,8 @@ const handleDiceRolling = async (player) => {
     waitP2.innerHTML = `${player02Data.effect}`
 
 
-    console.log('1 after', player01SkipTurns);
-    console.log('2 after', player02SkipTurns);
+    console.log('1 after', player01Data.effect);
+    console.log('2 after', player02Data.effect);
 
 
     currentPlayer = currentPlayer === 0 ? 1 : 0
@@ -125,6 +255,27 @@ const handleDiceRolling = async (player) => {
 const handlePostData = async (data) => {
   try {
     const response = await fetch('http://localhost:3000/move_player/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ data })
+    });
+    if (response.ok) {
+      result = await response.json()
+      console.log("update success");
+      return result
+    } else {
+      console.log("update fail");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const handleSkipTurns = async function updateEffect(data) {
+  try {
+    const response = await fetch('http://127.0.0.1:3000/effect_update/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
